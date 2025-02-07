76b67d1a1b919af4baea5dc0401b8c0867bc2854
JonCasey07 <jonathancasey2007@gmail.com>
2025-02-07 11:34:14 -0500

    camera following

--- Senior Project/Assets/Scripts/Player/CameraFollow.cs
+++ Senior Project/Assets/Scripts/Player/CameraFollow.cs
@@ -2,26 +2,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player; // Reference to the player's transform
-    public Vector3 offset; // Offset from the player's position
-    public float smoothSpeed = 1f; // Smoothing factor
-    public Transform leftBound;
-    public Transform rightBound;
-    public float fov = 15.36f;
+    [SerializeField] Transform player; // Reference to the player's transform
+    [SerializeField] float smoothSpeed; // Smoothing factor
+    [SerializeField] float leftBound;
+    [SerializeField] float rightBound;
 
     void LateUpdate()
     {
         // Only take into account the player's horizontal (x) movement
-        Vector3 desiredPosition = new Vector3(player.position.x + offset.x, transform.position.y, transform.position.z);
-        if(desiredPosition.x < leftBound.position.x + fov)
-        {
-            desiredPosition.x = leftBound.position.x + fov;
-        }
-        else if (desiredPosition.x > rightBound.position.x - fov)
-        {
-            desiredPosition.x = rightBound.position.x - fov;
-        }
+        Vector3 desiredPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        smoothedPosition = new Vector3 (Mathf.Clamp(smoothedPosition.x, leftBound, rightBound), smoothedPosition.y, smoothedPosition.z);
         transform.position = smoothedPosition;
     }
 }
