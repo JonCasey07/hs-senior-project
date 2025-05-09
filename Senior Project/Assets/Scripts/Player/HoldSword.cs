a13b7533320bc369382f007b215ab049a1674b3a
mhammond37 <159793082+mhammond37@users.noreply.github.com>
2025-05-09 10:45:29 -0400

    bald dude

--- Senior Project/Assets/Scripts/Player/HoldSword.cs
+++ Senior Project/Assets/Scripts/Player/HoldSword.cs
@@ -5,17 +5,26 @@ public class HoldSword : MonoBehaviour
     public Transform player;
     public PlayerController playerController;
     public Vector3 offset = Vector3.zero;
+    private Vector3 sword;
+
+    private void Start()
+    {
+        sword = transform.position;
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        sword.y = player.position.y + offset.y;
         if (playerController.facingRight)
         {
-            transform.position = player.position + offset;
+            sword.x = player.position.x + offset.x;
         }
         else
         {
-            transform.position = player.position - offset;
+            sword.x = player.position.x - offset.x;
         }
+
+        transform.position = sword;
     }
 }
