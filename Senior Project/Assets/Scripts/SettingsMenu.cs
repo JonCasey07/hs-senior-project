27d5298479acb1985f20a6b1644ccc1f293c2e83
JonCasey07 <jonathancasey2007@gmail.com>
2025-10-23 16:29:14 -0400

    added audio

--- Senior Project/Assets/Scripts/SettingsMenu.cs
+++ Senior Project/Assets/Scripts/SettingsMenu.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider Slider;
+    private float curVolume;
 
     public void Start()
     {
         Screen.fullScreen = true;
+        audioMixer.GetFloat("volume", out curVolume);
+        Slider.value = curVolume;
     }
     public void SetVolume(float volume)
     {
